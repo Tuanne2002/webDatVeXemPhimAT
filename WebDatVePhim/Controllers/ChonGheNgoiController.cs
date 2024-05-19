d1fb5692590a7b10bed69fce78dc47d747c3cc27
QuocAnh21033 <quocanh21033@gmail.com>
2024-05-19 16:30:01 +0700

    WebDatVePhim

---
+++ WebDatVePhim/Controllers/ChonGheNgoiController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebDatVePhim.Controllers
+{
+    public class ChonGheNgoiController : Controller
+    {
+        // GET: ChonGheNgoi
+        public ActionResult ChonGheNgoi()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebDatVePhim/Controllers/TrangAcountController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebDatVePhim.Controllers
+{
+    public class TrangAcountController : Controller
+    {
+        // GET: TrangAcount
+        public ActionResult TrangAcount()
+        {
+            return View();
+        }
+    }
+}
