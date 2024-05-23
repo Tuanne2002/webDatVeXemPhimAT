ffdc8f71c437eef5e874e70615cc0914f26511a4
Trương Đặng Anh Tuấn <truongdanganhtuan2002@gmail.com>
2024-05-24 00:03:04 +0700

    note ở BapNuoc.css line 16

--- WebDatVePhim/Models/Model1.Context.cs
+++ WebDatVePhim/Models/Model1.Context.cs
@@ -13,10 +13,10 @@ namespace WebDatVePhim.Models
     using System.Data.Entity;
     using System.Data.Entity.Infrastructure;
     
-    public partial class DatVeXemPhimATEntities : DbContext
+    public partial class DatVeXemPhimATEntities1 : DbContext
     {
-        public DatVeXemPhimATEntities()
-            : base("name=DatVeXemPhimATEntities")
+        public DatVeXemPhimATEntities1()
+            : base("name=DatVeXemPhimATEntities1")
         {
         }
     
@@ -34,7 +34,6 @@ namespace WebDatVePhim.Models
         public virtual DbSet<Phim> Phims { get; set; }
         public virtual DbSet<PhongChieu> PhongChieux { get; set; }
         public virtual DbSet<Rap> Raps { get; set; }
-        public virtual DbSet<sysdiagram> sysdiagrams { get; set; }
         public virtual DbSet<ThanhToan> ThanhToans { get; set; }
         public virtual DbSet<Ve> Ves { get; set; }
         public virtual DbSet<Ve_BapNuoc> Ve_BapNuoc { get; set; }
